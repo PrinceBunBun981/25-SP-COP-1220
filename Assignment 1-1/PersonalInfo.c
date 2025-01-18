892f239a6abf46ea4ba917c62aca0e42f9d14d53
Nick <nicholasalfonso981@gmail.com>
2025-01-18 15:16:59 -0500

    Assignment 1-1

---
+++ Assignment 1-1/PersonalInfo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    // Unit Test 1
+    printf("Lucinda Potter\n");
+    printf("6/24/1992\n");
+    printf("work 000-000-0101\n");
+    printf("cell 000-000-0189\n");
+
+    printf("\n");
+
+    // Unit Test 2
+    printf("John Smith\n");
+    printf("8/15/1978\n");
+    printf("work 000-111-0101\n");
+    printf("cell 000-111-0189\n");
+
+    printf("\n");
+
+    // Unit Test 3
+    printf("Chris Johnson\n");
+    printf("12/01/1987\n");
+    printf("work 000-222-0101\n");
+    printf("cell 000-222-0189\n");
+
+    printf("\n");
+
+    // Unit Test 4
+    printf("Robert Hall\n");
+    printf("2/27/1949\n");
+    printf("work 000-444-0101\n");
+    printf("cell 000-444-0189\n");
+
+    return 0;
+}
