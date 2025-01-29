7148e3b508387d8d298a99ce43d883a5680bebc7
Nick <nicholasalfonso981@gmail.com>
2025-01-29 16:23:02 -0500

    Assignment 2-1

---
+++ Assignment 2-1/Twitter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char tweet[1024];
+
+    printf("Enter a tweet: ");
+    scanf("%[^\n]", tweet);
+
+    if (strlen(tweet) <= 140) {
+        printf("Posted\n");
+    } else {
+        printf("Rejected\n");
+    }
+}   
